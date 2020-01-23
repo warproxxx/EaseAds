3899ee0e1d3e88916abf0b1c74618dfdb2fa6079
Daniel Sapkota <daniel@quantorithm.com>
2020-01-23 08:37:19 +0545

    Paypal kinda works. Now changes

---
+++ ads/application/config/paypal.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+It's highly recommended to use sandbox account for testing purpose
+*/
+
+$config['mode']          = "sandbox"; //for live account use the key "live"
+$config['client_id']     = 'AXa-FWWgZYHWeG_4VqQ_76e2lf5t7ueHuFtRONRXxThYOhjW1BP_QtWyjhdDJquHDPBi1W8ausmB0dQk'; //provide your client ID
+$config['client_secret'] = 'EKv2m4vXtX_zTxMZ_5FN1MmAca01zwx9bTwrQOFYI3dfsVzP9BCNOJxW6Qubt1Yi5xYzIPK3QxJpL3eG'; //provide your secret ID
+$config['currency']      = 'USD'; //provide the currency code you want to use
