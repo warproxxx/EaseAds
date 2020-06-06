c60af4bc4b8f325b9b62cd68896d5b4faa5e2d12
Daniel Sapkota <daniel@waterbot.xyz>
2020-06-06 16:20:31 +0545

    Ignore test

--- ads/application/config/database.php
+++ ads/application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost',ss
 	'username' => 'root',
 	'password' => 'E@seads1',
 	'database' => 'custch',
